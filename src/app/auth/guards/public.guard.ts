8293030877bcbc92c36393202a6adb076f7f5c89
doli.mia.fate.s@gmail.com <doli.mia.fate.s@gmail.com>
2024-12-16 15:27:34 +0100

    PublicGuard

--- src/app/auth/guards/public.guard.ts
+++ src/app/auth/guards/public.guard.ts
@@ -1,39 +1,32 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, CanMatch, Route, UrlSegment, GuardResult, MaybeAsync, RouterStateSnapshot, Router } from '@angular/router';
-import { Observable, tap } from 'rxjs';
+import { map, Observable, tap } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 
 @Injectable({providedIn: 'root'})
-export class AuthGuard implements CanMatch, CanActivate {
+export class PublicGuard implements CanMatch, CanActivate {
 
   constructor(
     private authService: AuthService,
     private router: Router
   ) { }
 
-  private checkAuthStatus(): boolean | Observable<boolean>  {
+  public checkAuthStatus(): boolean | Observable<boolean> {
     return this.authService.checkAuthentication()
       .pipe(
         tap(isAuthenticated => {
-          if(!isAuthenticated) {
-            this.router.navigate(['./auth/login'])
+          if(isAuthenticated) {
+            this.router.navigate(['./heroes/list'])
           }
-        })
+        }),
+        map(isAuthenticated => !isAuthenticated)
       )
   }
 
-  // En este caso, Can Match y Activate, usa como dato GuardResult que incluye tanto boolean, observable de boolean y UrlTree,
-  // cosa que este ultimo no voy a utilizar por lo que lo voy a cambiar
   canMatch(route: Route, segments: UrlSegment[]): boolean | Observable<boolean> {
-    // console.log('Can Match')
-    // console.log({route, segments})
-
     return this.checkAuthStatus();
   }
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | Observable<boolean> {
-    // console.log('Can Activate')
-    // console.log({route, state})
-
     return this.checkAuthStatus();
   }
 
