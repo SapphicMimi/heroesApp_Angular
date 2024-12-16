36ab32cb3e6b13383df48d2e7b351c5c073bc052
doli.mia.fate.s@gmail.com <doli.mia.fate.s@gmail.com>
2024-12-16 10:21:03 +0100

    AuthService v2

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -19,7 +19,7 @@ export class ServiceNameService {
 
   login(email: string, password: string): Observable<User> {
 
-    this.http.get<User>(`${this.baseUrl}/users/1`)
+    return this.http.get<User>(`${this.baseUrl}/users/1`)
       .pipe(
         tap(user => this.user = user),
         tap(user => localStorage.setItem('token', user.id.toString() ))
